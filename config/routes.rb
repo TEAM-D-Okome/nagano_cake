5861fc544e6afdfca50e99cda29cd4f6a8be65cb
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-17 10:09:03 +0000

    Add all_controller viewを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,38 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
   }
 
-  devise_for :customers
+  # 顧客用ルーティング
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  namespace :public do
+    get '/' => "homes#top"
+    get 'about' => 'homes#about'
+
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    patch 'customers/withdrawal' => 'customers#withdrawal'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+
+    resources :cart_items, only: [:index, :update, :create, :destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+    resources :orders, only: [:new, :index, :show, :create]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/finish' => 'orders#finish'
+
+    resources :delivery_addresses, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_items, only: [:update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
