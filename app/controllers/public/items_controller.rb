7b35fbdcb90ee49a4e0db31608e6280e351a4726
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-04-18 02:10:00 +0000

    public/items_controller記述

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.where(is_sale: true)
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.where(is_sale: true).page(params[:page]).reverse_order
+    else
+      @items = Item.where(is_sale: true).page(params[:page]).reverse_order
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.where(is_sale: true)
+    @cart_item = CartItem.new
   end
 end
