505f6bbbe39927d2509033392e5057593a8d127a
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-18 04:33:19 +0000

    Add CustomersControllerに記述を追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CustomersController < ApplicationController
-  def index
+  before_action :authenticate_admin! # ログイン済管理者のみにアクセスを許可する
+
+  def index #顧客一覧
+    @customers = Customer.page(params[:page]) #CustomerでCustomerモデルのデータを保持
+  end                                         #[.page]メソッドでデータをペジネーションするために使用
+                                              #(params[:page])で現在のページ番号を示すために使用されるパラメーター
+  def show #顧客詳細
+    @customer = Customer.find(params[:id])
   end
 
-  def show
+  def edit #顧客情報編集
+    @customer = Customer.find(params[:id])
   end
 
-  def edit
+  def update #顧客情報更新
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報の編集が完了しました。"
+      redirect_to admin_cutomer_path(@customer.id) #編集成功時はshow(会員詳細)へ遷移
+    else
+      render :edit #編集に失敗した場合は編集画面へ遷移
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :is_deleted)
   end
 end
