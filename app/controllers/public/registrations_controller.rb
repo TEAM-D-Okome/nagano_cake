e80fd9f4c2b69868e8be18f9bdf149d8a0debe3e
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-04-18 02:15:59 +0000

    不要な記述を削除

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,7 +3,6 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_up
   # def new
@@ -40,19 +39,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-
-  # 退会しているかを判断するメソッド
-  def customer_state
-    ## 【処理内容1】 入力されたemailからアカウントを1件取得
-    @customer = Customer.find_by(email: params[:customer][:email])
-    ## アカウントを取得できなかった場合、このメソッドを終了する
-    return if !@customer
-    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted == true
-      ## 【処理内容3】処理内容2がtrueかつcustomerのis_deletedがtrueならサインアップ画面に遷移
-      redirect_to new_customer_registration_path
-    end
-  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
