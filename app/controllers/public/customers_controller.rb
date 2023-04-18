b5319d73a1c1fac167579a47904142875e0f8410
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-04-18 02:35:27 +0000

    customersコントローラーに記述を追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,35 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customers.find(current_user.id)
   end
 
   def edit
+    @customer = Customers.find(current_user.id)
+  end
+
+  def update
+    @customer = Customers.find(current_user.id)
+    if @customer.update(customer_params)
+      redirect_to customer_path(current_user.id)
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+    @customer = Customers.find(current_user.id)
+  end
+
+  def withdrawal
+    @customer = Customers.find(current_user.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number)
   end
 end
