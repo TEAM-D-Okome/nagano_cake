6ae54eefb3af40fc2dcec618aac96df61d765503
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-19 08:59:30 +0000

    Fix 新規登録できないバグを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,19 +3,18 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
-  
-  validates :image, presence: true
+
   validates :name, presence: true
   validates :explanation, presence: true
   validates :tax_out_price, presence: true
   validates :is_sale, inclusion: [true, false]
-  
+
   # [tax_out_price]に対して1.10を掛けた値を四捨五入して返す
   # [add_tax]の部分が重要なので消してはいけない[消費税コード]と検索すれば出てくる
   def add_tax_tax_out_price
       (self.tax_out_price * 1.10).round
   end
-  
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join("app/assets/images/default-image.jpg")
@@ -23,5 +22,5 @@ class Item < ApplicationRecord
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
