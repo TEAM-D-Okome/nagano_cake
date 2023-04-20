88201a827cca02102d577c0f6f3014f9c4ebe08d
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-04-20 09:22:18 +0000

    view/public/cart_items/index記述

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,15 +3,16 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total_price = current_customer.cart_items.cart_items_total_price(@cart_items)
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
     if params[:cart_item][:quantity] == "0"
       @cart_item.destroy
-      redirect_to public_cart_items_path
+      redirect_to cart_items_path
     elsif @cart_item.update(quantity: params[:cart_item][:quantity])
-      redirect_to public_cart_items_path
+      redirect_to cart_items_path
     else
       @cart_items = current_customer.cart_items
       @total_price = @cart_items.inject(0) { |sum, item| sum + item.sum_price}
@@ -32,13 +33,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
-    current_customer.cart_items.find(params[:id]).destroy
-    redirect_to public_cart_items_path
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
     current_customer.cart_items.destroy_all
-    redirect_to public_cart_items_path
+    redirect_to cart_items_path
   end
 
   private
