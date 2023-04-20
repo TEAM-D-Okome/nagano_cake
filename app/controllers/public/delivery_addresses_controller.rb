5605cfb94d6be5b50a4d3571dd22a39b9cf20b98
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-04-20 06:35:20 +0000

    viewを作成

--- app/controllers/public/delivery_addresses_controller.rb
+++ app/controllers/public/delivery_addresses_controller.rb
@@ -1,15 +1,15 @@
 class Public::DeliveryAddressesController < ApplicationController
   def index
+    @new_delivery_address = DeliveryAddress.new
     @customer = current_customer
     @delivery_addresses = @customer.delivery_addresses
-    @new_delivery_address = DeliveryAddress.new
   end
 
   def create
     @delivery_addresses = DeliveryAddress.new(delivery_addresses_params)
     @delivery_addresses.customer_id = current_customer.id
     if @delivery_addresses.save
-      redirect_to delivery_addresses_path
+      redirect_to delivery_addresses_path, notice: "住所の登録に成功しました。"
     else
       @customer = current_customer
       @delivery_addresses = @customer.delivery_addresses
@@ -24,7 +24,7 @@ class Public::DeliveryAddressesController < ApplicationController
   def update
     @delivery_addresses = DeliveryAddress.find(params[:id])
     if @delivery_addresses.update(delivery_addresses_params)
-      redirect_to delivery_addresses_path
+      redirect_to delivery_addresses_path, notice: "編集に成功しました。"
     else
       render :edit
     end
@@ -34,11 +34,11 @@ class Public::DeliveryAddressesController < ApplicationController
     delivery_addresses = DeliveryAddress.find(params[:id])
     delivery_addresses.customer_id = current_customer.id
     delivery_addresses.destroy
-    redirect_to delivery_addresses_path
+    redirect_to delivery_addresses_path, notice: "削除に成功しました。"
   end
 
   private
   def delivery_addresses_params
-    params.require(:delivery_addresses).permit(:post_code, :address, :name)
+    params.require(:delivery_address).permit(:post_code, :address, :name)
   end
 end
