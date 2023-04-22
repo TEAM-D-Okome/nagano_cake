b29eb6f842703dddb3d22c7f888d88039cc64ba9
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-04-22 21:10:02 +0000

    ジャンルサイドバーの実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all.order(creatad_at: :desc).page(params[:page]).per(8)
-    @genres = Genre.where(is_sale: true)
+    @genres = Genre.all
     if params[:genre_id]
       @genre = Genre.find(params[:genre_id])
       @items = @genre.items.where(is_sale: true).page(params[:page]).reverse_order
@@ -12,13 +12,13 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @genres = Genre.where(is_sale: true)
+    @genres = Genre.all
     @cart_item = CartItem
   end
-  
+
   private
   def item_params
     params.require(:items).permit(:genre_id,:name,:explanation,:image,:tax_out_price, :is_sa)
   end
-  
+
 end
