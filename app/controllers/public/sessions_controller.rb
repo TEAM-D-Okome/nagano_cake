8fd74f47beeb730e33608d336adc8c0605fedcae
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-04-24 05:51:33 +0000

    ログイン後に遷移する画面とユーザー情報の編集を修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -21,7 +21,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    root_path
+    customer_path(current_customer)
   end
 
   def after_sign_out_path_for(resource)
